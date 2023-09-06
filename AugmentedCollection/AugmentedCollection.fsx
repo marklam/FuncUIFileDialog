41fb1865494f86ea530df45cb48e053cbfbc1453
Mark Lambert <marklam@spunow.co.uk>
2023-09-06 16:09:55 +0100

    Awesome solution from @JaggerJo - https://github.com/fsprojects/Avalonia.FuncUI/discussions/345#discussioncomment-6926289

--- AugmentedCollection/AugmentedCollection.fsx
+++ AugmentedCollection/AugmentedCollection.fsx
@@ -55,47 +55,51 @@ module Counter =
     let itemTemplate (annotations : IReadable<_>) dispatch =
         printfn "itemTemplate constructed with annotations version %d" annotations.Current.Version
         fun (item : Item) ->
-            if item.Index = 0 then printfn "itemTemplate invoked for item %d with annotations version %d" item.Index annotations.Current.Version
-            Grid.create [
-                Grid.height 80
-                Grid.columnDefinitions "Auto,*"
-                Grid.rowDefinitions "*,*"
-                Grid.children [
-                    TextBlock.create [
-                        Grid.row 0
-                        Grid.column 0
-                        TextBlock.text "Index"
-                        TextBlock.background Brushes.LightBlue
-                        TextBlock.horizontalAlignment HorizontalAlignment.Stretch
-                        TextBlock.verticalAlignment VerticalAlignment.Center
+            Component.create ("item", (
+                fun ctx -> 
+                    let annotations = ctx.usePassedRead annotations
+                    if item.Index = 0 then printfn "itemTemplate invoked for item %d with annotations version %d" item.Index annotations.Current.Version
+                    Grid.create [
+                        Grid.height 80
+                        Grid.columnDefinitions "Auto,*"
+                        Grid.rowDefinitions "*,*"
+                        Grid.children [
+                            TextBlock.create [
+                                Grid.row 0
+                                Grid.column 0
+                                TextBlock.text "Index"
+                                TextBlock.background Brushes.LightBlue
+                                TextBlock.horizontalAlignment HorizontalAlignment.Stretch
+                                TextBlock.verticalAlignment VerticalAlignment.Center
+                            ]
+                            TextBlock.create [
+                                Grid.row 0
+                                Grid.column 1
+                                TextBlock.text (string item.Index)
+                                TextBlock.verticalAlignment VerticalAlignment.Center
+                            ]
+                            TextBlock.create [
+                                Grid.row 1
+                                Grid.column 0
+                                TextBlock.text "Notes"
+                                TextBlock.background Brushes.LightBlue
+                                TextBlock.horizontalAlignment HorizontalAlignment.Stretch
+                                TextBlock.verticalAlignment VerticalAlignment.Center
+                            ]
+                            TextBox.create [
+                                let note =
+                                    Map.tryFind item.Index annotations.Current.Notes
+                                    |> Option.defaultValue ""
+                                Grid.row 1
+                                Grid.column 1
+                                TextBox.background Brushes.Yellow
+                                TextBox.text note
+                                TextBox.onTextChanged (fun note -> SetNote (item.Index, note) |> dispatch)
+                                TextBox.verticalAlignment VerticalAlignment.Center
+                            ]
+                        ]
                     ]
-                    TextBlock.create [
-                        Grid.row 0
-                        Grid.column 1
-                        TextBlock.text (string item.Index)
-                        TextBlock.verticalAlignment VerticalAlignment.Center
-                    ]
-                    TextBlock.create [
-                        Grid.row 1
-                        Grid.column 0
-                        TextBlock.text "Notes"
-                        TextBlock.background Brushes.LightBlue
-                        TextBlock.horizontalAlignment HorizontalAlignment.Stretch
-                        TextBlock.verticalAlignment VerticalAlignment.Center
-                    ]
-                    TextBox.create [
-                        let note =
-                            Map.tryFind item.Index annotations.Current.Notes
-                            |> Option.defaultValue ""
-                        Grid.row 1
-                        Grid.column 1
-                        TextBox.background Brushes.Yellow
-                        TextBox.text note
-                        TextBox.onTextChanged (fun note -> SetNote (item.Index, note) |> dispatch)
-                        TextBox.verticalAlignment VerticalAlignment.Center
-                    ]
-                ]
-            ]
+            ))
 
     let annotatedList dispatch (selPeakGroup : IWritable<Item option>) (peaksList : ImmutableList<Item>) (annotations : IReadable<Augmentations>) =
         printfn "List rebuilt with annotations %d" annotations.Current.Version
